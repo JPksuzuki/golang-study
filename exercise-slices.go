dbd068931381ecc95abf6f648387dc6b9062e2a8
JPksuzuki <suzuki.kei@nifty.co.jp>
2017-04-12 15:50:35 +0900

    演習２
    スライスの演習解答です

---
+++ exercise-slices.go
@@ -0,0 +1,24 @@
+﻿package main
+
+import (
+	"golang.org/x/tour/pic"
+	//"math"
+)
+
+func Pic(dx, dy int) [][]uint8 {
+	var picture [][]uint8
+	for i := 0; i < dx; i++{	
+		var tmp_1Dslice []uint8
+		for j := 0; j < dy; j++{
+			tmp_1Dslice = append( tmp_1Dslice, uint8((i + j)/2) )		
+			//tmp_1Dslice = append( tmp_1Dslice, uint8(i * j) )
+			//tmp_1Dslice = append( tmp_1Dslice, uint8(math.Pow(float64(i), float64(j))) )
+		}
+		picture = append(picture, tmp_1Dslice)
+	}
+	return picture
+}
+
+func main() {
+	pic.Show(Pic)
+}
