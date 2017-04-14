3c5d1653477149bfdefbc382c6d9582152d96490
JPksuzuki <suzuki.kei@nifty.co.jp>
2017-04-14 18:18:44 +0900

    演習３
    マップの演習解答です

--- exercise-maps.go
+++ exercise-maps.go
@@ -6,17 +6,17 @@ import (
 )
 
 func WordCount(s string) map[string]int {
-	var want = make(map[string]int) //戻り値
+	var want = make(map[string]int)
 
 	var words []string = strings.Fields(s) //単語ごとに分解
 
-	for _, word := range words { //各単語について
-		_, ok := want[word] //単語に対する要素が存在するか
-		if !ok {
+	for _, word := range words {
+		_, isExist := want[word] //単語に対する要素が存在するか
+		if !isExist {
 			want[word] = 1 //存在しない場合 → wordの数を1に初期化
 		} else {
 			want[word]++ //存在する場合 → wordの数を+1
-		} 
+		}
 	}
 	return want
 }
