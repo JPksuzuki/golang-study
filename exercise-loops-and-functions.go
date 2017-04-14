0996e062d1f13ece67af9ff666f58799f22938c3
JPksuzuki <suzuki.kei@nifty.co.jp>
2017-04-14 18:10:53 +0900

    演習１_v2
    ループと関数の演習解答その２です

--- exercise-loops-and-functions.go
+++ exercise-loops-and-functions.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 )
 
-var iter int = 1000
-var delta float64 = 0.00000000001
+const iter int = 1000
+const delta float64 = 1e-15
 
 func myAbs(x float64) float64 {
-	if x < 0 { return -x }
+	if x < 0 {
+		return -x
+	}
 	return x
 }
 
@@ -16,17 +18,17 @@ func Sqrt(x float64) float64 {
 	z := 1.0
 	count := 0
 	for n := 0; n < iter; n++ {
-		count++		
-		z_tmp := z
-		z = z - (z * z - x)/(2 * z)
-		if myAbs(z - z_tmp) < delta {
+		count++
+		prev_z := z
+		z = z - (z*z-x)/(2*z)
+		if myAbs(z - prev_z) < delta {
 			fmt.Println(count)
 			return z
-		} 
+		}
 	}
 	return z
 }
 
 func main() {
-	fmt.Println("\n",Sqrt(100))
+	fmt.Println("\n", Sqrt(100))
 }
