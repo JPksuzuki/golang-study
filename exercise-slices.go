30f344f6f44eaad31484f7ff3f7b10d350ff2d05
JPksuzuki <suzuki.kei@nifty.co.jp>
2017-04-19 19:11:45 +0900

    演習２
    スライスの演習解答です

--- exercise-slices.go
+++ exercise-slices.go
@@ -7,12 +7,12 @@ import (
 
 func Pic(dx, dy int) [][]uint8 {
 	var picture [][]uint8
-	for i := 0; i < dx; i++{	
-		var tmp_1Dslice []uint8
-		for j := 0; j < dy; j++{
-			tmp_1Dslice = append( tmp_1Dslice, uint8((i + j)/2) )		
-			//tmp_1Dslice = append( tmp_1Dslice, uint8(i * j) )
-			//tmp_1Dslice = append( tmp_1Dslice, uint8(math.Pow(float64(i), float64(j))) )
+	for i := 0; i < dx; i++ {
+		var row []uint8
+		for j := 0; j < dy; j++ {
+			row = append(row, uint8((i+j)/2))
+			//row = append( row, uint8(i * j) )
+			//row = append( row, uint8(math.Pow(float64(i), float64(j))) )
 		}
 		picture = append(picture, tmp_1Dslice)
 	}
